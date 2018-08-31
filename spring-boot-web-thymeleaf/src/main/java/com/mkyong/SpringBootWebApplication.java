8fcf1df32974d6f17f1af4e180072b69616a21c5
Srikant <Srikant@sana.hsd1.ca.comcast.net>
2018-08-30 21:46:10 -0700

    added sysout

--- spring-boot-web-thymeleaf/src/main/java/com/mkyong/SpringBootWebApplication.java
+++ spring-boot-web-thymeleaf/src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -10,6 +10,7 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	System.out.println("");
         return application.sources(SpringBootWebApplication.class);
     }
 
